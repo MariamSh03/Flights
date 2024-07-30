3e7bbac661f3665525ff7035eb538774ca9620e7
Mariam <mshonia18@gmail.com>
2024-07-30 23:53:27 +0400

    first commit

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,26 +6,20 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { SearchFlightComponent } from './search-flight/search-flight.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    SearchFlightComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: SearchFlightComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
---
+++ ClientApp/src/app/search-flight/search-flight.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flight',
+  templateUrl: './search-flight.component.html',
+  styleUrls: ['./search-flight.component.css']
+})
+export class SearchFlightComponent implements OnInit {
+
+  searchResult: any = [
+    "American Airlains",
+    "British Airways",
+    "Georgian Airways",
+    "Turkish Airlines"
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+
+}
