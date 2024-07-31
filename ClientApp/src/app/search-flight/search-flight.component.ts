7beec3bad55acf4c421d47309a818059eb568734
Mariam <mshonia18@gmail.com>
2024-07-31 16:55:25 +0400

    completed user interface

--- ClientApp/src/app/search-flight/search-flight.component.ts
+++ ClientApp/src/app/search-flight/search-flight.component.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,12 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFlightComponent implements OnInit {
 
-  searchResult: any = [
-    "American Airlains",
-    "British Airways",
-    "Georgian Airways",
-    "Turkish Airlines"
-  ]
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNuimberOfSeats: 500,
+      departure: { time: new Date().toString(), place: "Los Angeles" },
+      arival: { time: new Date().toString(), place: "Istanbul" },
+      price: "350",
+    },
+    {
+      airline: "Turkish Airlines",
+      remainingNuimberOfSeats: 300,
+      departure: { time: new Date().toString(), place: "Ankara" },
+      arival: { time: new Date().toString(), place: "Istanbul" },
+      price: "200",
+    },
+    {
+      airline: "Lufthansa",
+      remainingNuimberOfSeats: 400,
+      departure: { time: new Date().toString(), place: "Berlin" },
+      arival: { time: new Date().toString(), place: "Frankfurt" },
+      price: "250",
+    },
+    {
+      airline: "Turkish Airlines",
+      remainingNuimberOfSeats: 150,
+      departure: { time: new Date().toString(), place: "Antalya" },
+      arival: { time: new Date().toString(), place: "Izmir" },
+      price: "180",
+    },
+    {
+      airline: "Lufthansa",
+      remainingNuimberOfSeats: 350,
+      departure: { time: new Date().toString(), place: "Düsseldorf" },
+      arival: { time: new Date().toString(), place: "Munich" },
+      price: "220",
+    }
+  ];
 
   constructor() { }
 
@@ -20,4 +52,18 @@ export class SearchFlightComponent implements OnInit {
    
   }
 
+}
+
+export interface FlightRm {
+  airline: string;
+  arival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNuimberOfSeats: number;
+
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
